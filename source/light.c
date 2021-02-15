92fc2146c90e0ca88a7ff1e6d93dbfd3394b5edd
janannij <jananni.johanraj@gmail.com>
2021-02-15 16:33:32 +0100

    Start of lights

---
+++ source/light.c
@@ -0,0 +1,5 @@
+#include "light.h"
+
+void light_init(){
+    
+}
--- source/light.h
+++ source/light.h
@@ -1,6 +1,6 @@
 /**
  * @file light.h
- * @author your name (you@domain.com)
+ * @author Ask & Sivaranjith
  * @brief 
  * @version 0.1
  * @date 2021-02-15
@@ -13,7 +13,38 @@
 #define LIGHT.H
 
 
+#include "hardware.h"
+#include <stdint.h>
 
+#define NUMBER_OF_LIGHTS{15}
 
+/***/
+typedef struct
+{
+    uint8_t floor_light_state[];
+    uint8_t stop_light_state;
+} Lights;
+
+/**
+ * @brief 
+ * 
+ */
+void light_init();
+
+/**
+ * @brief Set the floor light object
+ * 
+ * @param floor 
+ * @param order_type 
+ * @param on 
+ */
+void set_floor_light(int floor, HardwareOrder order_type, uint8_t on);
+
+/**
+ * @brief Set the stop light object
+ * 
+ * @param on 
+ */
+void set_stop_light(uint8_t on);
 
 #endif
