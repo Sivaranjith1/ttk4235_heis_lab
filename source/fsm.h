ec76f0901827140e79b23a07659b175ab0c8a312
Ranjith <ranjith1210000@gmail.com>
2021-02-23 07:29:50 +0100

    :art: Refactored fsm

--- source/fsm.h
+++ source/fsm.h
@@ -26,17 +26,17 @@ void fsm_init();
  * @brief gets the current state of the finite state machine
  * @returns STATE the state of the current state
 */
-STATE getFsmState();
+STATE get_fsm_state();
 
 /**
  * @brief sets the current state to the newState
  * @param newState the newState to set current state equal to
 */
-void setFsmState(STATE newState);
+void set_fsm_state(STATE newState);
 
 /**
  * @brief A blocking function running the finite state machine
 */
-void fsmRun();
+void fsm_run();
 
 #endif
