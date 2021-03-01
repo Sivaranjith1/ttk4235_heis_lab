c2fa314ec6799234f40fcf21d7b7d8b53637fe56
Sivaranjith Sivarasa <ranjith1210000@gmail.com>
2021-03-01 14:02:18 +0100

    Added light and added clear function

---
+++ source/light.c
@@ -0,0 +1,17 @@
+#include "light.h"
+#include "hardware.h"
+
+void light_init(){
+    HardwareOrder order_types[3] = {
+        HARDWARE_ORDER_UP,
+        HARDWARE_ORDER_INSIDE,
+        HARDWARE_ORDER_DOWN
+    };
+
+    for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
+        for(int i = 0; i < 3; i++){
+            HardwareOrder type = order_types[i];
+            hardware_command_order_light(f, type, 0);
+        }
+    }
+}
---
+++ source/light.h
@@ -0,0 +1,17 @@
+/**
+ * @file lights.h
+ * @author Ask and Sivaranjith
+ * @brief 
+ * @version 0.1
+ * @date 2021-03-01
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef LIGHT_H
+#define LIGHT_H
+
+void light_init();
+
+#endif
