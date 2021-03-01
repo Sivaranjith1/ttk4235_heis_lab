049b73cdc1d7823767a6d47f2da35a60bfe42fbd
Sivaranjith Sivarasa <ranjith1210000@gmail.com>
2021-03-01 14:07:16 +0100

    Clear all lights on floor

--- source/light.c
+++ source/light.c
@@ -1,17 +1,25 @@
 #include "light.h"
 #include "hardware.h"
 
-void light_init(){
-    HardwareOrder order_types[3] = {
+static HardwareOrder order_types[3] = {
         HARDWARE_ORDER_UP,
         HARDWARE_ORDER_INSIDE,
         HARDWARE_ORDER_DOWN
-    };
+};
 
+void light_init(){
     for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
         for(int i = 0; i < 3; i++){
             HardwareOrder type = order_types[i];
             hardware_command_order_light(f, type, 0);
         }
+    }
+}
+
+
+void light_clear_all_on_floor(uint8_t floor){
+    for(int i = 0; i < 3; i++){
+        HardwareOrder type = order_types[i];
+        hardware_command_order_light(floor, type, 0);
     }
 }
--- source/light.h
+++ source/light.h
@@ -12,6 +12,20 @@
 #ifndef LIGHT_H
 #define LIGHT_H
 
+#include <stdint.h>
+
+/**
+ * @brief Resets all the lights on different floors
+ * 
+ */
 void light_init();
+
+
+/**
+ * @brief Clears all the lights on one floor
+ * 
+ * @param floor The floor to clear the lights on
+ */
+void light_clear_all_on_floor(uint8_t floor);
 
 #endif
