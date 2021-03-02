0b5dba19b82f55b04f252171229058c31bd52e25
Sivaranjith Sivarasa <ranjith1210000@gmail.com>
2021-03-02 15:12:02 +0100

    Added updated button

---
+++ button.c
@@ -0,0 +1,96 @@
+#include "button.h"
+
+uint8_t button_poll_floor(){
+    for(uint8_t i = 0; i < HARDWARE_NUMBER_OF_FLOORS; i++){
+        if(hardware_read_order(i, HARDWARE_ORDER_DOWN)){
+            return i;
+        }
+        else if(hardware_read_order(i, HARDWARE_ORDER_UP)){
+            return i;
+        }
+        else if(hardware_read_order(i, HARDWARE_ORDER_INSIDE)){
+            return i;
+        }
+    }
+    return HARDWARE_NUMBER_OF_FLOORS; // non-existant floor
+}
+
+HardwareOrder button_poll_order(){
+    for(int i = 0; i < HARDWARE_NUMBER_OF_FLOORS; i++){
+        if(hardware_read_order(i, HARDWARE_ORDER_DOWN)){
+            return HARDWARE_ORDER_DOWN;
+        }
+        else if(hardware_read_order(i, HARDWARE_ORDER_UP)){
+            return HARDWARE_ORDER_UP;
+        }
+        else if(hardware_read_order(i, HARDWARE_ORDER_INSIDE)){
+            return HARDWARE_ORDER_INSIDE;
+        }
+    }
+    return 3; // non-existant order
+}
+OrderDirection button_find_order_direction(HardwareOrder order_type){
+    OrderDirection return_direction;
+    if(order_type == HARDWARE_ORDER_DOWN){
+        return_direction = DIRECTION_DOWN;
+    }
+    else if (order_type == HARDWARE_ORDER_UP)
+    {
+        return_direction = DIRECTION_UP;
+    }
+    else{
+        return_direction = DIRECTION_INSIDE; // should never happen
+    }
+
+    return return_direction;
+}
+
+void button_on_stop_button_press(){
+    hardware_command_movement(HARDWARE_MOVEMENT_STOP);
+    queue_clear();
+}
+
+void button_on_obstruction_press(time_t* current_time){
+}
+
+uint8_t button_check_buttons_pressed(){
+   int stop_pressed = hardware_read_stop_signal();
+   int obstruction_pressed = hardware_read_obstruction_signal();
+
+
+   if(stop_pressed) {
+        return STOP_BUTTON_PRESSED;
+   }
+
+   else
+   {
+       for(int i = 0; i < HARDWARE_NUMBER_OF_FLOORS; i++){
+            if(hardware_read_order(i, HARDWARE_ORDER_DOWN)) return EXTERNAL_ORDER_EXISTS;
+            else if(hardware_read_order(i, HARDWARE_ORDER_UP)) return EXTERNAL_ORDER_EXISTS;
+            else if(hardware_read_order(i, HARDWARE_ORDER_INSIDE)) return INTERNAL_ORDER_EXISTS;
+       }
+   }
+
+   if(obstruction_pressed){
+        return OBSTRUCTION_BUTTON_PRESSED;
+   }
+
+    return 0;
+}
+
+void button_on_external_order_button_press(){
+    uint8_t floor = button_poll_floor();
+    if(floor >= HARDWARE_NUMBER_OF_FLOORS) return;
+    HardwareOrder order_type = button_poll_order();
+    OrderDirection direction = button_find_order_direction(order_type);
+    queue_add_element(floor, PRIORITY_OUTSIDE, direction);
+    hardware_command_order_light(floor, order_type, 1);   
+}
+
+void button_on_internal_order_button_press(){
+    uint8_t floor = button_poll_floor();
+    if (floor >= HARDWARE_NUMBER_OF_FLOORS)
+        return;
+    queue_add_element(floor, PRIORITY_INSIDE, DIRECTION_INSIDE);
+    hardware_command_order_light(floor, HARDWARE_ORDER_INSIDE, 1);
+}
---
+++ button.h
@@ -0,0 +1,76 @@
+/**
+ * @file button.h
+ * @author Ask and Sivaranjith
+ * @brief File for button controls and interactions
+ * @version 0.1
+ * @date 2021-02-15
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include "hardware.h"
+#include "queue.h"
+#include <time.h>
+#include <stdint.h>
+
+#define STOP_BUTTON_PRESSED 1
+#define OBSTRUCTION_BUTTON_PRESSED 2
+#define EXTERNAL_ORDER_EXISTS 3
+#define INTERNAL_ORDER_EXISTS 4
+/**
+ * @brief Handles the stop button. When called it stops the elevator, clears the queue of all orders.
+ * 
+ */
+void button_on_stop_button_press();
+
+
+/**
+ * @brief 
+ * 
+ */
+void button_on_obstruction_press();
+
+/**
+ * @brief 
+ * 
+ * @param order_type 
+ * @return OrderDirection
+ */
+OrderDirection button_find_order_direction(HardwareOrder order_type);
+
+/**
+ * @brief 
+ * 
+ * @param floor 
+ * @param order_type 
+ */
+void button_on_external_order_button_press();
+
+
+/**
+ * @brief 
+ * 
+ * @param floor 
+ */
+void button_on_internal_order_button_press();
+
+
+/**
+ * @brief 
+ * 
+ */
+uint8_t button_poll_floor();
+
+
+/**
+ * @brief 
+ * 
+ * @return uint8_t 
+ */
+uint8_t button_check_buttons_pressed();
+#endif
