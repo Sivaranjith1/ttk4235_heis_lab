05971f8ca6d2915e7b85829ead15e61e731ba286
Ranjith <ranjith1210000@gmail.com>
2021-03-02 05:42:07 +0100

    Removed duplicateded entries

--- source/fsm.c
+++ source/fsm.c
@@ -50,6 +50,7 @@ STATE get_fsm_state()
 
 void set_fsm_state(STATE newState)
 {
+    printf("Changing state to %d\n", newState);
     current_under_state = EXIT;
     fsm_run_inner();
 
@@ -164,7 +165,9 @@ static void fsm_waiting_state()
                 go_to_floor(next_floor->toFloor);
 
                 if(set_last_visited_floor() == MOVEMENT_STILL){
+                    printf("Deleting current floor\n");
                     queue_delete_orders_at_floor(get_last_visited_floor());
+                    print_all_floor_orders();
                 }
             }
 
@@ -292,13 +295,6 @@ static void fsm_on_floor_reached(){
     uint8_t current_floor = (uint8_t) get_last_visited_floor();
     queue_delete_orders_at_floor(current_floor);
     light_clear_all_on_floor(current_floor);
-    queue_add_element(floor_temp, PRIORITY_INSIDE, DIRECTION_INSIDE);
-    queue_add_element(floor_temp, PRIORITY_INSIDE, DIRECTION_INSIDE);
-    queue_add_element(floor_temp, PRIORITY_INSIDE, DIRECTION_INSIDE);
-    queue_add_element(floor_temp, PRIORITY_INSIDE, DIRECTION_INSIDE);
-    queue_add_element(floor_temp++, PRIORITY_INSIDE, DIRECTION_INSIDE);
-
-    if(floor_temp == 4) floor_temp = 0;
 
     set_fsm_state(WAITING);
 }
